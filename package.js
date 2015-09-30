1b19bdc5d87bd0942b8e305f084eef3a58b1bfe4
Max Ferguson <maxkferg@gmail.com>
2015-09-30 16:18:05 -0700

    Added files

---
+++ package.js
@@ -0,0 +1,22 @@
+Package.describe({
+  name: 'maxkferg:cdn',
+  version: '1.0.0',
+  summary: 'Serve Meteor content from a CDN',
+  git: 'https://github.com/NitroLabs/metepr',
+  documentation: 'README.md'
+});
+
+Package.onUse(function(api) {
+  api.versionsFrom('1.1.0.2');
+  api.use('webapp','server');
+  api.use('templating','client');
+  api.addFiles('lib/template.js','client');
+  api.addFiles('lib/client.js','client');
+  api.addFiles('lib/server.js','server');
+});
+
+Package.onTest(function(api) {
+  api.use('tinytest');
+  api.use('maxkferg:cdn');
+  api.addFiles('cdn-tests.js');
+});
