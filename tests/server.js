d391554443fdaa5011d67c78e804f21c86a18b8a
Max Ferguson <maxkferg@gmail.com>
2016-01-15 12:56:01 -0800

    Added missing semicolons

--- tests/server.js
+++ tests/server.js
@@ -3,7 +3,7 @@ var path = Npm.require("path");
 
 
 // These tests directly influence the Meteor server that
-// is running them. Therefore the ROOT_URL and 
+// is running them. Therefore the ROOT_URL and
 // CDN_URL must be reset after every test
 function resetState(){
   CDN._setCdnUrl(process.env.CDN_URL);
@@ -19,7 +19,7 @@ Tinytest.add(
     var cdn = "http://www.cloudfront.com/s9ufe3u2rns/";
     var root1 = "https://www.meteor.com/e9043utosn/";
     var root2 = "http://www.meteor.com/e9043utosn/";
-    
+
     result = CDN._validateSettings(root1,cdn);
     test.isTrue(result, "validateSettings should return True if the settings are valid");
 
@@ -37,7 +37,7 @@ Tinytest.add(
   function (test) {
     cdn = "https://www.cloudfront.com/s9ufe3u2rns/";
     root = "https://www.meteor.com/e9043utosn/";
-    
+
     result = CDN._validateSettings(undefined,cdn);
     test.isFalse(result, "validateSettings should return False if the settings are insufficient");
 
@@ -62,7 +62,7 @@ Tinytest.add(
     rootValid = "http://www.meteor.com/e9043utosn/"
     rootInvalid1 = "ddp://www.meteor.com/e9043utosn/";
     rootInvalid2 = "www.meteor.com/e9043utosn/"
-    
+
     test.throws(function(){
       CDN._validateSettings(rootInvalid1,cdnValid);
     }, "ROOT_URL must use http or https protocol, not ddp:");
@@ -104,7 +104,7 @@ Tinytest.add(
   function (test) {
   	cdn = "http://www.cloudfront.com/s9ufe3u2rns/";
   	root = "http://www.meteor.com/e9043utosn/";
-  	CDN._setRootUrl(root)
+  	CDN._setRootUrl(root);
     CDN._setCdnUrl(cdn);
 
     test.equal(__meteor_runtime_config__.CDN_URL, cdn, 'Expected Meteor environment to be setup correctly');
@@ -119,7 +119,7 @@ var req = { headers: {}};
 
 var res = { status:0,
             headers: {},
-            write: function(){}, 
+            write: function(){},
             end: function(){},
             writeHead:function(status){
               this.status = status;
@@ -146,9 +146,9 @@ Tinytest.add(
     var root = "http://www.meteor.com/e9043utosn/";
     var staticUrl1 = root + "myimages.js";
     var staticUrl2 = root + "images/myImage.js";
-    CDN._setRootUrl(root)
+    CDN._setRootUrl(root);
     CDN._setCdnUrl(cdn);
-    
+
     req.url = staticUrl1;
     req.headers.host = root;
     res.status = 200;
@@ -175,7 +175,7 @@ Tinytest.add(
     var root = "http://www.meteor.com/";
     CDN._setRootUrl(root)
     CDN._setCdnUrl(cdn);
-    
+
     req.url = root;
     req.headers.host = root;
     res.status = 200;
@@ -195,13 +195,13 @@ Tinytest.add(
     var status;
     var cdn = "https://www.cloudfront.com/";
     var root = "https://www.meteor.com/";
-    CDN._setRootUrl(root)
+    CDN._setRootUrl(root);
     CDN._setCdnUrl(cdn);
-    
+
     req.url = cdn;
     req.headers.host = url.parse(cdn).host;
     res.nextCalls = nextCalls;
-    
+
     CDN._static404connectHandler(req,res,next);
     test.equal(res.status,404);
     test.equal(nextCalls,res.nextCalls);
@@ -221,9 +221,9 @@ Tinytest.add(
     var root = "https://www.meteor.com/";
     var staticUrl1 = cdn + "myimages.js";
     var staticUrl2 = cdn + "images/myImage.js";
-    CDN._setRootUrl(root)
+    CDN._setRootUrl(root);
     CDN._setCdnUrl(cdn);
-    
+
     req.url = staticUrl1;
     req.headers.host = url.parse(cdn).host;
     res.nextCalls = nextCalls;
@@ -253,10 +253,10 @@ Tinytest.add(
     var cdn = "https://www.cloudfront.com/";
     var root = "http://www.meteor.com/";
     var staticUrl = cdn + "packages/underscore.js";
-    
+
     CDN._setRootUrl(root)
     CDN._setCdnUrl(cdn);
-    
+
     req.url = staticUrl;
     req.headers.host = url.parse(cdn).host;
     res.status = 200;
@@ -287,13 +287,13 @@ Tinytest.add(
     fonts.push(fixtures + "icomoon.svg");
     fonts.push(fixtures + "icomoon.ttf");
     fonts.push(fixtures + "icomoon.woff");
-    
+
     CDN._setRootUrl(root)
     CDN._setCdnUrl(cdn);
 
     req.headers.host = url.parse(cdn).host;
     res.status = 200;
-    
+
     for (var i=1; i<fonts.length; i++){
       req.url = fonts[i];
       res.headers = {};
@@ -322,14 +322,14 @@ Tinytest.add(
     var root = "http://www.meteor.com/";
     var fixtures = cdn + "packages/local-test_maxkferg_cdn/tests/fixtures/";
     var font = fixtures + "icomoon.woff?-acxumy";
-    
+
     CDN._setRootUrl(root)
     CDN._setCdnUrl(cdn);
 
     req.url = font
     req.headers.host = url.parse(cdn).host;
     res.headers = {};
-    
+
     CDN._CORSconnectHandler(req,res,next);
     test.equal(res.headers['Strict-Transport-Security'],'max-age=2592000; includeSubDomains','Missing STS Header')
     test.equal(res.headers['Access-Control-Allow-Origin'], '*', 'Missing ACAO Header');
@@ -349,16 +349,16 @@ Tinytest.add(
     var root = "http://www.meteor.com/";
     var fixtures = cdn + "packages/local-test_maxkferg_cdn/tests/fixtures/";
     var filepath = fixtures + "icomoon.css";
-    
-    CDN._setRootUrl(root)
+
+    CDN._setRootUrl(root);
     CDN._setCdnUrl(cdn);
 
-    req.url = filepath
+    req.url = filepath;
     req.headers.host = url.parse(cdn).host;
     res.headers = {};
-    
+
     CDN._CORSconnectHandler(req,res,next);
-    test.equal(res.headers['Strict-Transport-Security'], undefined, 'Extra STS Header')
+    test.equal(res.headers['Strict-Transport-Security'], undefined, 'Extra STS Header');
     test.equal(res.headers['Access-Control-Allow-Origin'], undefined, 'Extra ACAO Header');
     resetState()
   }
