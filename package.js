9586a09e0753418ea2f72b6c9cedd3625830862d
Max Ferguson <maxkferg@gmail.com>
2016-01-16 00:23:17 -0800

    v1.2.3

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   name: 'nitrolabs:cdn',
-  version: '1.2.2',
+  version: '1.2.3',
   summary: 'Serve Meteor content from a CDN',
   git: 'https://github.com/nitrolabs/meteor-cdn',
   documentation: 'README.md'
