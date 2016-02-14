c1730ad75b585e27849751af6b768099abcfa740
Kyoto Ishii <kyoto.ishii@gmail.com>
2016-02-13 16:38:00 -0800

    v 1.2.4

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   name: 'nitrolabs:cdn',
-  version: '1.2.3',
+  version: '1.2.4',
   summary: 'Serve Meteor content from a CDN',
   git: 'https://github.com/nitrolabs/meteor-cdn',
   documentation: 'README.md'
